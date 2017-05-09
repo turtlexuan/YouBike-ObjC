fd59e80097b4da4f3195c273645f71bf1af1ac32
turtlexuan <turtlexuan@gmail.com>
2017-05-09 10:50:42 +0800

    1. Install Pods.

---
+++ YouBike-ObjC/Pods/Target Support Files/Pods-YouBike-ObjC/Pods-YouBike-ObjC-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_YouBike_ObjC : NSObject
+@end
+@implementation PodsDummy_Pods_YouBike_ObjC
+@end
---
+++ YouBike-ObjC/Pods/Target Support Files/Pods-YouBike-ObjC/Pods-YouBike-ObjC-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_YouBike_ObjCVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_YouBike_ObjCVersionString[];
+
