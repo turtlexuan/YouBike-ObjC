ce88f176e852f78012ce915b8ad7b6891708f7d6
turtlexuan <turtlexuan@gmail.com>
2017-05-09 18:09:30 +0800

    1. ProfileVC view settings. 2. ProfileVC safariVC.

--- YouBike/YouBike/Controller/ProfileViewController.m
+++ YouBike/YouBike/Controller/ProfileViewController.m
@@ -8,7 +8,8 @@
 
 #import "ProfileViewController.h"
 
-@interface ProfileViewController ()
+@interface ProfileViewController () <SFSafariViewControllerDelegate>
+
 @property (weak, nonatomic) IBOutlet UIView *backgroundView;
 @property (weak, nonatomic) IBOutlet UIImageView *backgroundImageView;
 @property (weak, nonatomic) IBOutlet UIView *gradientView;
@@ -23,15 +24,70 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)viewDidAppear:(BOOL)animated{
+    [super viewDidAppear:animated];
+    
+    [self settingView];
+    
 }
 
 - (IBAction)homePageAction:(id)sender {
+    
+    NSString *urlString = [[NSUserDefaults standardUserDefaults] objectForKey:@"link"];
+    
+    if (urlString != nil) {
+        
+        NSURL *url = [NSURL URLWithString:urlString];
+        SFSafariViewController *safariVC = [[SFSafariViewController alloc] initWithURL:url];
+        safariVC.delegate = self;
+        [self presentViewController:safariVC animated:true completion:nil];
+        
+    }
+    
 }
+
+
+- (void)settingView {
+    
+    self.gradientView.layer.cornerRadius        = self.gradientView.layer.frame.size.width / 2;
+    self.profileImageView.layer.cornerRadius    = self.profileImageView.layer.frame.size.width / 2;
+    self.homePageButtonView.layer.cornerRadius  = 10;
+    
+    self.gradientView.layer.masksToBounds       = true;
+    self.profileImageView.layer.masksToBounds   = true;
+    self.homePageButtonView.layer.masksToBounds = true;
+    
+    CAShapeLayer *rectShape;
+    rectShape.bounds = self.backgroundView.frame;
+    rectShape.position = self.backgroundView.center;
+    rectShape.path = [UIBezierPath bezierPathWithRoundedRect: self.backgroundView.bounds
+                                           byRoundingCorners:UIRectCornerBottomLeft | UIRectCornerBottomRight
+                                                 cornerRadii:CGSizeMake(20, 20)].CGPath;
+    
+    self.backgroundView.layer.mask = rectShape;
+    
+    NSString *username = [[NSUserDefaults standardUserDefaults] objectForKey:@"name"];
+    
+    if (username != nil) {
+        
+        self.usernameLabel.text = username;
+    }
+    
+    NSString *pictureUrlString = [[NSUserDefaults standardUserDefaults] objectForKey:@"picture"];
+    
+    if (pictureUrlString != nil) {
+        
+        NSURL *url = [NSURL URLWithString:pictureUrlString];
+        NSData *data = [NSData dataWithContentsOfURL:url];
+        self.profileImageView.image = [UIImage imageWithData:data];
+        
+    }
+    
+}
+
 
 /*
 #pragma mark - Navigation
